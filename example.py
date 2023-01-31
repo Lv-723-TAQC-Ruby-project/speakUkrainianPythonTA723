b246fb4e47f1aa4580f295a8ea3e1a0fe54579cb
Liubomyr Halamaha <galamagal86@gmail.com>
2023-01-31 14:52:27 +0200

    edded exampls

---
+++ example.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("https://speak-ukrainian.org.ua/dev/")
+btn =driver.find_element(by=By.XPATH, value='//*[@id="root"]/section/header/div[2]/ul/li[1]')
+btn.click()
+time.sleep(10)
+div = driver.find_element(by=By.XPATH, value='//*[@id="root"]/section/section/main/section/section/main/div/div[1]/div/div/div[1]/div[2]')
+print(div.text)
+
+driver.quit()
