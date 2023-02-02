292868a41b8a92caadcce3ff717e419faa3b3504
simpleUA <a.lipskiy@yahoo.com>
2023-02-02 14:58:38 +0200

    fixed settings files

--- settings/settings.py
+++ settings/settings.py
@@ -2,6 +2,6 @@ base_web_url = "enter base_web_url"
 admin_email = "enter admin_email"
 admin_password = "enter admin_password"
 try:
-    from local_settings import *
+    from settings.local_settings import *
 except:
     pass
